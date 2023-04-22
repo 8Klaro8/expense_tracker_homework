0e99f742703e4ecbacdd8ef3133fb9acf6f187a1
Robin Gerg <gergrobin@gmail.com>
2023-04-22 17:02:44 +0200

    added new function
    convert_currency has been added plus test

--- main.py
+++ main.py
@@ -8,19 +8,29 @@ from fixed_api_key import fixed_api_key
 class ExpenseTracker:
     def __init__(self) -> None:
         # self.my_request = MyRequest()
-        pass
+        self.headers = {"apikey": fixed_api_key}
+        
 
     def get_HUF_base_USD(self, base: str, symbol: str) -> str:
         """ Gets the current value of one currency compare to an other"""
-
         url = f"https://api.apilayer.com/fixer/latest?base={base}&symbols={symbol}"
-        headers = {"apikey": fixed_api_key}
-        response = requests.get(url=url, headers=headers)
+        response = requests.get(url=url, headers=self.headers)
+        return response.text
+    
+    
+    def convert_currency(self, base: str, convert_to: str, amount: str) -> str:
+        url = f"https://api.apilayer.com/fixer/convert?from={base}&to={convert_to}& \
+                amount={amount}"
+        response = requests.get(url=url, headers=self.headers)
         return response.text
     
     def convert_str_to_json(self, value: str) -> json:
         """ Converts given value to dict """
-
         json_result =  json.loads(value)
         return json_result
-    
+
+
+if __name__ == '__main__':
+    e_t = ExpenseTracker()
+    result = e_t.convert_currency("USD", "HUF", "5")
+    print(result)
--- test.py
+++ test.py
@@ -7,16 +7,22 @@ class MyTest(unittest.TestCase):
 
     def setUp(self) -> None:
         self.expense_tracker = ExpenseTracker()
+        self.examp_currs = ("USD", "HUF")
+        self.amount = "6"
 
     # MAIN.PY
     def test_get_conversion(self):
-        result = self.expense_tracker.get_HUF_base_USD("USD", "HUF")
+        result = self.expense_tracker.get_HUF_base_USD(*self.examp_currs)
         self.assertIsInstance(result, str)
 
     def test_convert_str_to_json(self):
-        result = self.expense_tracker.get_HUF_base_USD("USD", "HUF")
+        result = self.expense_tracker.get_HUF_base_USD(*self.examp_currs)
         json_result = self.expense_tracker.convert_str_to_json(result)
         self.assertIsInstance(json_result, dict)
+
+    def test_convert_currency(self):
+        result = self.expense_tracker.convert_currency(*self.examp_currs, self.amount)
+        self.assertIsInstance(result, str)
 
 if __name__ == '__main__':
     unittest.main()
